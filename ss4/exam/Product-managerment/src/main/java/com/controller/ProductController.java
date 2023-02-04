46fa528a64153717dda52440f71cb3a8b799501e
phanvanhung1995 <phanvanhung1224@gmail.com>
2023-02-04 17:07:10 +0700

    bai_tap

--- ss4/exam/Product-managerment/src/main/java/com/controller/ProductController.java
+++ ss4/exam/Product-managerment/src/main/java/com/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,4 +37,29 @@ public class ProductController {
         return "redirect:/";
     }
 
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id,Model model) {
+        Product product = productService.findById(id);
+        model.addAttribute("product",product);
+        return "/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(Product product) {
+        productService.edit(product.getId(),product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id, RedirectAttributes redirect) {
+        productService.delete(id);
+        redirect.addFlashAttribute("mess","xóa thành công");
+        return "redirect:/";
+    }
+
+    @GetMapping("/view/{id}")
+    public String showProduct(@PathVariable int id,Model model) {
+        model.addAttribute("product",productService.findById(id));
+        return "/view";
+    }
 }

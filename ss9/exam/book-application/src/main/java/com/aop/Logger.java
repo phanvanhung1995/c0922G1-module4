ba271528179b9a76df8c100a2cdf9e4ba0a91e3a
phanvanhung1995 <phanvanhung1224@gmail.com>
2023-02-13 21:14:04 +0700

    bai_tap

--- ss9/exam/book-application/src/main/java/com/aop/Logger.java
+++ ss9/exam/book-application/src/main/java/com/aop/Logger.java
@@ -1,5 +1,6 @@
 package com.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
@@ -7,4 +8,26 @@ import org.springframework.stereotype.Component;
 @Component
 @Aspect
 public class Logger {
+    private static int count = 0;
+    private static int count1 = 0;
+
+    @Pointcut("execution(* com.controller.BookController.borrow(..))" + "||execution(* com.controller.BookController.pay(..))")
+    public void getAllBorrowBook() {
+    }
+
+    @After(value = "getAllBorrowBook()")
+    public void print() {
+        count++;
+        System.out.println("mượn hoặc trả sách ần thứ  : " + count);
+    }
+
+    @Pointcut("execution(* com.controller.BookController.*())")
+    public void numberOfHits() {
+    }
+
+    @After(value = "numberOfHits()")
+    public void print2() {
+        count1++;
+        System.out.println("số lượt người ghé thăm thứ : " + count1);
+    }
 }
